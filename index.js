383e7031a6eaf05da5e68d59e07e47c92c15b84f
JacquelineMG <j.gilchrist@outlook.com>
2023-05-19 15:14:03 -0700

    implement nextISSTimesForMyLocation chained function

---
+++ index.js
@@ -0,0 +1,53 @@
+const { nextISSTimesForMyLocation } = require('./iss');
+
+// console logs ISS fly over times in an easily readable format
+const printPassTimes = function(passTimes) {
+  for (const p of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(p.risetime);
+    const duration = p.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
+// calls function chain from iss.js and uses printPassTimes function to print times
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  printPassTimes(passTimes);
+});
+
+
+
+// Additional test functions:
+
+/*
+fetchMyIP((error, ip) => {
+  if (error) {
+    console.log("It didn't work!", error);
+    return;
+  }
+  return ip;
+});
+
+const myIP = "70.66.168.54";
+
+fetchCoordsByIP(myIP, (error, coordinates) => {
+  console.log("Error Output:", error);
+  console.log("Data Output:", coordinates);
+});
+
+const myCoords = { latitude: '48.435842', longitude: '-123.4112341' };
+
+fetchISSFlyOverTimes(myCoords, (error, response) => {
+  if (error) {
+    console.log("It didn't work!", error);
+    return;
+  }
+  console.log("It worked!", response);
+});
+*/
+
+
+
---
+++ iss.js
@@ -0,0 +1,97 @@
+const request = require('request');
+
+// retrieves IP address from ipify.org
+const fetchMyIP = function(callback) {
+  const url = "https://api.ipify.org?format=json";
+  request(url, (error, response, body) => {
+    if (error) return callback(error, null);
+      
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+    const ip = JSON.parse(body).ip;
+    callback(null, ip);
+  });
+};
+
+// uses ip to retrieve coordinates from ipwho.is
+const fetchCoordsByIP = function(ip, callback) {
+  const url = `http://ipwho.is/${ip}`;
+  request(url, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    const parsedBody = JSON.parse(body);
+
+    if (parsedBody.success === false) {
+      const msg = `Success status was ${parsedBody.success}. Server message says: ${parsedBody.message} when fetching for IP ${parsedBody.ip}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    const lat = parsedBody.latitude;
+    const lon = parsedBody.longitude;
+    const coordinates = {
+      latitude: String(lat),
+      longitude: String(lon)
+    };
+    callback(null, coordinates);
+  });
+};
+
+// uses coordinates to retrieve next ISS fly over times
+const fetchISSFlyOverTimes = function(coords, callback) {
+  const url = `https://iss-flyover.herokuapp.com/json/?lat=${coords.latitude}&lon=${coords.longitude}`;
+ 
+  request(url, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    const resp = JSON.parse(body).response;
+    callback(null, resp);
+  });
+};
+
+
+// chains the above functions to return fly over times
+const nextISSTimesForMyLocation = function(callback) {
+  fetchMyIP((error, ip) => {
+    if (error) {
+      return callback(error, null);
+    }
+
+    fetchCoordsByIP(ip, (error, coordinates) => {
+      if (error) {
+        return callback(error, null);
+      }
+
+      fetchISSFlyOverTimes(coordinates, (error, passTimes) => {
+        if (error) {
+          return callback(error, null);
+        }
+
+        callback(null, passTimes);
+      });
+    });
+  });
+};
+
+module.exports = { nextISSTimesForMyLocation };
